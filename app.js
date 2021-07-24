e48f5009dc750bb7b90b5d496e14ed004bc19baa
lucasrod1 <lucasfarina97@gmail.com>
2021-07-24 15:01:06 -0300

    Se agregaron las imagnes al proyecto y se empezo a trabajar en el HTML

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express()
 const port = 3030
 const path = require('path')
 
+app.use(express.static('public'))
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'views/index.html'))
+})
 
 app.listen(port, () =>{
     console.log('La pagina esta funcionando en http://localhost:' + port)
