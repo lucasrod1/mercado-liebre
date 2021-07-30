3fa2c89344f2cf7cd78af1780074c14a6f096978
lucasrod1 <lucasfarina97@gmail.com>
2021-07-30 01:46:06 -0300

    Se finaliza el trabajo hasta el momento, agregando los microdesafios hasta la fecha

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
+const publicPath = path.join(__dirname, 'public');
 const port = 3030
-const path = require('path')
 
-app.use(express.static('public'))
+app.use(express.static(publicPath));
 
-app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'views/index.html'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/index.html'))
 })
 
-app.listen(port, () =>{
-    console.log('La pagina esta funcionando en http://localhost:' + port)
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
+})
+
+app.listen(port, () => {
+    console.log("El servidor se encuentra activo sobre la URL: http://localhost:"+port+" o sobre http://127.0.0.1:"+port)
 })
